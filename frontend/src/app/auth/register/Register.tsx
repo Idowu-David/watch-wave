f214271ff338da52691a4df4d725ce7279a02cf6
Lilian Ekechukwu <lilianekechukwuc@gmail.com>
2025-12-16 19:31:42 +0100

    added globals.css

---
+++ frontend/src/app/auth/register/Register.tsx
@@ -0,0 +1,14 @@
+// src/pages/register.tsx (or wherever your file is)
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="bg-purple-500 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  );
+};
+
+export default Register;
