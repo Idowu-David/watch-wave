96888820a1fd0134fcf40b8dc38074c5e874553d
Kesington Abdulwakeel <mr.abdulwakeel@yahoo.com>
2025-12-17 21:26:07 +0100

    feat: add update watchlist route with error handling

--- backend/src/routes/watchlist.ts
+++ backend/src/routes/watchlist.ts
@@ -85,4 +85,15 @@ router.get("/getWatchListStatus/:userId/", async (req: Request, res: Response) =
         });
     }
 })
+
+router.put("/updateWatchlist/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params; 
+    } catch (error) {
+        return res.status(500).json({   
+            message: "Server error",
+            error,
+        }); 
+    }
+});
 export default router;
